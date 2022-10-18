a9afc5d3d35f1999baaa336226211462e1691a59
jonathanjonathan001 <jonathanrosemark@gmail.com>
2022-10-18 12:59:58 +0200

    add buttons and stuff

--- src/main/java/labb3jhr/labb3/DrawController.java
+++ src/main/java/labb3jhr/labb3/DrawController.java
@@ -3,7 +3,7 @@ package labb3jhr.labb3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class DrawController {
 //    @FXML
 //    private Label welcomeText;
 
